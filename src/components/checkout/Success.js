4197d5e3010666dcb584a8d6b9be392d4e77deb2
williammacinnis <will@macinnisweb.ca>
2024-07-15 22:37:07 -0300

    Success.js updated.

--- src/components/checkout/Success.js
+++ src/components/checkout/Success.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './checkout.css';
-import {useLocation, useParams} from 'react-router-dom'; // Assuming you use React Router
+import { useLocation } from 'react-router-dom'; // Assuming you use React Router
 import axios from 'axios'; // Use Axios or fetch API for making HTTP requests
 
 const Success = () => {
@@ -8,12 +8,19 @@ const Success = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const session_id = searchParams.get('session_id');
-    console.log(session_id);
+
+    console.log('Session ID:', session_id);
+
     useEffect(() => {
         const fetchCheckoutInfo = async () => {
+            if (!session_id) {
+                console.error('Session ID is null or undefined');
+                return;
+            }
+
             try {
                 const response = await axios.get(`${process.env.REACT_APP_API_URL}/checkout/checkout-session/${session_id}`, {
-                    headers: { 'authorization':process.env.REACT_APP_API_KEY }
+                    headers: { 'authorization': process.env.REACT_APP_API_KEY }
                 });
                 setCheckoutInfo(response.data);
             } catch (error) {
@@ -38,13 +45,11 @@ const Success = () => {
                     <p className="title">Checkout complete! </p>
                     <p className="subtitle">You will receive your ticket and receipt via email soon. </p>
                     <br/>
-                        <p><small>Session ID: {checkoutInfo.sessionId}</small></p>
+                    <p><small>Session ID: {checkoutInfo.sessionId}</small></p>
 
                 </div>
             </div>
-
         </section>
-
     );
 };
 
